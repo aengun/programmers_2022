991d1f6ec1f29aa0666b263b6aae9cf28488b785
aengun <aengun@icloud.com>
2022-06-27 23:30:24 +0900

    Lv1 x만큼 간격이 있는 n개의 숫자

---
+++ src/Level1/x만_간격이_있는_n개의_숫자/Solution.java
@@ -0,0 +1,13 @@
+package Level1.x만_간격이_있는_n개의_숫자;
+
+class Solution {
+    public long[] solution(int x, int n) {
+        long[] answer = new long[n];
+        long tmp = x;
+        for (int i = 0; i < n; i++) {
+            answer[i] = tmp;
+            tmp += x;
+        }
+        return answer;
+    }
+}
