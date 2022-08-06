de585d999249cca0c778e8084e579d1e0cbc87c0
aengun <aengun@icloud.com>
2022-08-06 20:17:39 +0900

    Lv2 행렬 테두리 회전하기 주석 추가

--- src/Level2/행렬_테두_회전하기/Solution.java
+++ src/Level2/행렬_테두_회전하기/Solution.java
@@ -21,24 +21,28 @@ class Solution {
             int y1 = queries[i][1] - 1;
             int x2 = queries[i][2] - 1;
             int y2 = queries[i][3] - 1;
-            int tmp = arr[x1][y1];
+            int tmp = arr[x1][y1]; // 시작점은 삭제되므로 미리 저장
             int min = tmp;
 
+            // 아래 방향으로 가면서 차례로 값을 채운다
             for (int x = x1; x < x2; x++) {
                 arr[x][y1] = arr[x + 1][y1];
                 min = Math.min(arr[x + 1][y1], min);
             }
 
+            // 우측 방향
             for (int y = y1; y < y2; y++) {
                 arr[x2][y] = arr[x2][y + 1];
                 min = Math.min(arr[x2][y + 1], min);
             }
 
+            // 위쪽 방향
             for (int x = x2; x > x1; x--) {
                 arr[x][y2] = arr[x - 1][y2];
                 min = Math.min(arr[x - 1][y2], min);
             }
 
+            // 좌측 방향
             for (int y = y2; y > y1; y--) {
                 arr[x1][y] = arr[x1][y - 1];
                 min = Math.min(arr[x1][y - 1], min);
