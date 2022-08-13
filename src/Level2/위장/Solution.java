5fa6a0ab3024037e828fb849e8e7ba345c683514
aengun <aengun@icloud.com>
2022-08-13 21:39:08 +0900

    Lv2 위장

---
+++ src/Level2/위장/Solution.java
@@ -0,0 +1,39 @@
+package Level2.위장;
+
+import java.util.*;
+
+class Solution {
+    public int solution(String[][] clothes) {
+        int answer = 1;
+
+        HashMap<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < clothes.length; i++) {
+            String kind = clothes[i][1];
+            map.put(kind, map.getOrDefault(kind, 0) + 1);
+        }
+
+        for (String key : map.keySet()) {
+            answer *= map.get(key) + 1;
+        }
+        answer--;
+        return answer;
+    }
+}
+
+// import java.util.HashMap;
+// import java.util.Map;
+// class Solution {
+//     public int solution(String[][] clothes) {
+//         int answer = 1;
+
+// 		Map<String, Integer> map = new HashMap<>();
+// 		for (int i = 0; i < clothes.length; i++) {
+// 			map.put(clothes[i][1], map.getOrDefault(clothes[i][1], 0) + 1);
+// 		}
+
+// 		for (String key : map.keySet())
+// 			answer *= map.get(key) + 1;
+
+// 		return answer - 1;
+//     }
+// }
