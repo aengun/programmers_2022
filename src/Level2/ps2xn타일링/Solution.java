80fc4414c4aa6d4c1b83ec831fe414d8dcc8f4ab
aengun <aengun@icloud.com>
2022-08-13 21:23:55 +0900

    Lv2 2 x N 타일링 다시풀기

--- src/Level2/ps2xn타일링/Solution.java
+++ src/Level2/ps2xn타일링/Solution.java
@@ -1,24 +1,40 @@
 package Level2.ps2xn타일링;
 
+//22.08.13 다시풀기
 class Solution {
     public int solution(int n) {
         int answer = 0;
 
-        int dp[] = new int[60001];
-        dp[1] = 1;
-        dp[2] = 2;
-        if (n == 1) return 1;
-        if (n == 2) return 2;
-
+        int[] arr = new int[60001];
+        arr[1] = 1;
+        arr[2] = 2;
         for (int i = 3; i <= n; i++) {
-            dp[i] = (dp[i - 1] + dp[i - 2]) % 1000000007;
+            arr[i] = (arr[i - 1] + arr[i - 2]) % 1000000007;
         }
-
-        answer = dp[n];
-
+        answer = arr[n];
         return answer;
     }
 }
+
+//class Solution {
+//    public int solution(int n) {
+//        int answer = 0;
+//
+//        int dp[] = new int[60001];
+//        dp[1] = 1;
+//        dp[2] = 2;
+//        if (n == 1) return 1;
+//        if (n == 2) return 2;
+//
+//        for (int i = 3; i <= n; i++) {
+//            dp[i] = (dp[i - 1] + dp[i - 2]) % 1000000007;
+//        }
+//
+//        answer = dp[n];
+//
+//        return answer;
+//    }
+//}
 
 // class Solution {
 //     public int solution(int n) {
